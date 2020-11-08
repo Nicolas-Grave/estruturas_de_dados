8c9809401176dd54d80a731f8e4e6304cb7ebc19
Nicolas Grave <nyko-23@hotmail.com>
2020-11-08 00:02:11 -0300

    Add metrics complete, solution and partial to PackingContainersLiquidItems.

--- PackingContainersLiquidItems.java
+++ PackingContainersLiquidItems.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-class Project2Task2 {
+class PackingContainersLiquidItems {
   static class Item {
     public String description;
     public double volume;
@@ -23,18 +23,77 @@ class Project2Task2 {
       this.items = new ArrayList<>();
     }
 
+    public void addItem(Item item) {
+      items.add(new Item(item.description, item.volume));
+    }
+
     public double volumeUsed() {
       double volume = 0;
       for (Item item : items) {
         volume += item.volume;
       }
-      return volume;
+      return (double) Math.round(volume * 100.0) / 100.0;
     }
   }
 
-  static List<Container> bestContainers;
+  static class PackContainer {
+    List<Container> containers;
+    double maxContainerVolume;
+
+    public PackContainer(double maxContainerVolume, List<Item> items) {
+      this.maxContainerVolume = maxContainerVolume;
+      this.containers = buildContainersByItems(items);
+    }
+
+    public List<Container> buildContainersByItems(List<Item> items) {
+      List<Container> containers = new ArrayList<>();
+
+      Container container = new Container();
+      containers.add(container);
+
+      for(Item item : items) {
+        if (item.volume > this.maxContainerVolume) break;
+
+        double volume = (container.volumeUsed() + item.volume);
+        volume = Math.round(volume * 100.0) / 100.0;
+
+        if (volume > this.maxContainerVolume) {
+          container = new Container();
+          containers.add(container);
+        }
+
+        container.addItem(item);
+      }
+
+      return containers;
+    }
+
+    private Container lastContainer() {
+      if (containers == null || containers.isEmpty()) {
+        return null;
+      } else {
+        return containers.get(containers.size() - 1);
+      }
+    }
+
+    public boolean isBetterThan(PackContainer packContainer) {
+      return (containers.size() < packContainer.containers.size()) ||
+        (containers.size() == packContainer.containers.size() && lastContainer().volumeUsed() < packContainer.lastContainer().volumeUsed());
+    }
+
+    public boolean allContainersIsFull() {
+      double emptySpace = 0;
+      for (Container container : containers) {
+        emptySpace += (container.volumeUsed() - maxContainerVolume);
+      }
+      return emptySpace == 0;
+    }
+  }
+
+  static PackContainer bestPackContainer;
   static List<Item> items;
-  static double containerVolume;
+  static double maxContainerVolume;
+  static int complete, partial, solution;
 
   public static void main(String[] args) {
     setDefaultValues();
@@ -48,57 +107,42 @@ class Project2Task2 {
 
     solve(new ArrayList<>());
     displayContainers();
+
+    System.out.println("partial:  " + partial);
+    System.out.println("complete: " + complete);
+    System.out.println("solution: " + solution);
   }
 
   private static void solve(List<Item> solveItems) {
     if (solveItems.size() == items.size()) {
-      List<Container> containers = buildSolveContainers(solveItems);
+      solution++;
+      PackContainer packContainer = new PackContainer(maxContainerVolume, solveItems);;
 
-      if (bestContainers.size() == 0 || bestContainers.size() > containers.size()) {
-        bestContainers = containers;
-      } else if (bestContainers.size() == containers.size() && bestContainers.get(bestContainers.size() - 1).volumeUsed() > containers.get(containers.size() - 1).volumeUsed()) {
-        bestContainers = containers;
-      }
+      if (packContainer.allContainersIsFull()) complete++;
+
+      if (packContainer.isBetterThan(bestPackContainer)) bestPackContainer = packContainer;
     } else {
       for (Item item : items) {
-        if (!listContainerItem(solveItems, item)) {
+        if (!listContainsItem(solveItems, item)) {
           List<Item> nextSolveItems = new ArrayList<>(solveItems);
           nextSolveItems.add(new Item(item.description, item.volume));
+          partial++;
           solve(nextSolveItems);
         }
       }
     }
   }
 
-  private static boolean listContainerItem(List<Item> items, Item item) {
+  private static boolean listContainsItem(List<Item> items, Item newItem) {
     boolean containsItem = false;
-    for (Item _item : items) {
-      if (_item.description.equals(item.description)) containsItem = true;
+    for (Item item : items) {
+      if (item.description.equals(newItem.description)) containsItem = true;
     }
     return containsItem;
   }
 
-  private static List<Container> buildSolveContainers(List<Item> items) {
-    List<Container> containers = new ArrayList<>();
-    containers.add(new Container());
-
-    for(Item item : items) {
-      Container lastContainer = containers.get(containers.size() - 1);
-
-      if ((lastContainer.volumeUsed() + item.volume) > containerVolume) {
-        containers.add(new Container());
-        lastContainer = containers.get(containers.size() - 1);
-      }
-
-      lastContainer.items.add(new Item(item.description, item.volume));
-    }
-
-    return containers;
-  }
-
   private static void setDefaultValues() {
-    bestContainers = new ArrayList<>();
-    containerVolume = 3.3;
+    maxContainerVolume = 3.3;
 
     items = new ArrayList<>();
     items.add(new Item("1", 1.5));
@@ -106,13 +150,15 @@ class Project2Task2 {
     items.add(new Item("3", 2.3));
     items.add(new Item("4", 1.8));
     items.add(new Item("5", 0.9));
+
+    bestPackContainer = new PackContainer(maxContainerVolume, items);
   }
 
   private static void displayContainers() {
-    System.out.println(bestContainers.size() + " containers were used");
+    System.out.println(bestPackContainer.containers.size() + " containers were used");
     System.out.println();
-    for(int i = 0; i < bestContainers.size(); i++) {
-      Container container = bestContainers.get(i);
+    for(int i = 0; i < bestPackContainer.containers.size(); i++) {
+      Container container = bestPackContainer.containers.get(i);
       System.out.println("Container #" + (i + 1) + " volume used: " + container.volumeUsed());
       for(Item item : container.items) {
         System.out.println("--- Item " + item.description + " volume " + item.volume);
